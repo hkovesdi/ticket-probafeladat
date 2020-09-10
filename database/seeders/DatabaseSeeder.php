fd64cfac9a45f7f1ccdb83f8b077ef8e2226e826
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-10 15:01:49 +0200

    Create customer in databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\Customer;
+use \App\Models\Ticket;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        Customer::factory()->hasTickets(3)->create();
     }
 }
