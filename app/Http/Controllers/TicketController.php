cab21aaedfd170c6851e9a9d5d58c999b4d62bbd
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-14 12:50:33 +0200

    create tickets page

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\Ticket;
 
 class TicketController extends Controller
 {
     public function index()
-    {
-        return view('tickets');
+    {   
+        $tickets = Ticket::with('customer')->paginate(20);
+        return view('tickets', ['tickets' => $tickets]);
     }
 
     public function create() 
     {
         return view('create-ticket');
+    }
+
+    public function store(Request $request) 
+    {
+
     }
 }
--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -1,5 +1,28 @@
 @extends('app')
 
 @section('content')
-tickets
+<div class="container" style="margin-top: 2rem;">
+    <div class="row">
+        {{$tickets->links('vendor.pagination.default')}}
+    @foreach($tickets as $ticket)
+        <div class="col s12 m6 l4 xl3">
+          <div class="card medium">
+            <div class="card-content black-text">
+              <span class="card-title">{{$ticket->title}}</span>
+              <p>{{$ticket->content}}</p>
+              <br/>
+              <p style=""> <b>Beküldve:</b> {{$ticket->created_at}}</p>
+              <p style=""> <b>Beküldő:</b> {{$ticket->customer->name}}</p>
+            </div>
+            <div class="card-action">
+                Esedékes:
+                <div class="chip">
+                    {{$ticket->due_date}}
+                </div>
+            </div>
+          </div>
+        </div>
+    @endforeach
+    </div>
+</div>
 @endsection
