6f26f7401443004f5b655f24ca28752745cc6cf4
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-14 15:11:13 +0200

    create StoreTicket request

---
+++ app/Http/Requests/StoreTicket.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTicket extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user() === null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|unique:customers|email',
+            'title' => 'required|max:255',
+            'content' => 'required|max:2000'
+        ];
+    }
+}
