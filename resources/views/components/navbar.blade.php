f4e0e98fc935356b3905416c1ab2fd9bfde0fb9c
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-14 15:10:26 +0200

    modify navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,30 @@
  <nav class="teal lighten-2">
         <div class="nav-wrapper">
         <a href="#" class="brand-logo">Ticket rendszer</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+        <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right: 1rem">
+            @auth
+                <li class="{{request()->route()->getName() == 'tickets.index' ? 'active' : ''}}">
+                    <a href="{{route('tickets.index')}}">Hibajegyek</a>
+                </li>
+
+                <li class="{{request()->route()->getName() == 'customers.index' ? 'active' : ''}}">
+                    <a href="{{route('customers.index')}}">Ügyfelek</a></li>
+                <li>
+                    <a href="javascript:{}" onclick="document.getElementById('logout-form').submit();">Kijelentkezés</a>
+                    <form id="logout-form" method="POST" action="{{route('logout')}}">
+                        @csrf
+                    </form>
+                </li>
+                <li>{{auth()->user()->username}}</li>
+            @endauth
+            @guest
+                <li class="{{request()->route()->getName() == 'tickets.create' ? 'active' : ''}}">
+                    <a href="{{route('tickets.create')}}">Új hibajegy</a>
+                </li>
+                <li class="{{request()->route()->getName() == 'login' ? 'active' : ''}}">
+                    <a href="{{route('login')}}">Bejelentkezés</a>
+                </li>
+            @endguest
         </ul>
         <a href="#" data-target="slide-out" class="sidenav-trigger left hide-on-lg-and-up"><i class="material-icons">menu</i></a>
     </div>
