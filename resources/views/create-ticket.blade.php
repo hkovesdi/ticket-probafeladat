cbf441fbdbfa25fad381f3f043954b6133204713
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-14 15:10:54 +0200

    add action to create ticket form

--- resources/views/create-ticket.blade.php
+++ resources/views/create-ticket.blade.php
@@ -6,39 +6,40 @@
       <div class="card">
         <div class="card-content">
           <span class="card-title black-text">Új hibajegy beküldése</span>
-          <form>
+          <form id="create-ticket-form" method="POST" action="{{route('tickets.store')}}">
+            @csrf
             <div class="row">
               <div class="input-field col s12">
                   <i class="material-icons prefix grey-text text-darken-2">account_circle</i>
-                  <input placedholder="Név" id="name" type="text" class="validate">
+                  <input name="name" id="name" type="text" class="validate">
                   <label for="name">Név</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
                   <i class="material-icons prefix grey-text text-darken-2">email</i>
-                  <input id="email" type="email" class="">
+                  <input name="email" id="email" type="email" class="">
                   <label for="email">Email</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
                   <i class="material-icons prefix grey-text text-darken-2">edit</i>
-                  <input id="subject" type="text" class="validate">
-                  <label for="subject">Tárgy</label>
+                  <input name="title" id="title" type="text" class="validate">
+                  <label for="title">Tárgy</label>
               </div>
             </div>
             <div class="row" style="margin-bottom: 0px">
               <div class="input-field col s12">
                   <i class="material-icons prefix grey-text text-darken-2">textsms</i>
-                  <textarea id="content" class="materialize-textarea"></textarea>
+                  <textarea name="content" id="content" class="materialize-textarea"></textarea>
                   <label for="content">Tartalom</label>
               </div>
             </div>
           </form>
         </div>
         <div class="card-action right-align">
-          <button type="submit" class="btn waves-effect waves-light">Beküldés</button>
+          <button type="submit" form="create-ticket-form" class="btn waves-effect waves-light">Beküldés</button>
         </div>
       </div>
 @endsection
