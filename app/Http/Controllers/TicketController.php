8dbf5ca8ca0c9696886b5e2dc0d14bc2354fb15f
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 10:45:02 +0200

    use SortTicket request in index of TicketController

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -8,34 +8,11 @@ use \App\Models\Ticket;
 class TicketController extends Controller
 {   
 
-    /**
-     * Process the query string
-     * @param array $options The possible values the $queryString can have
-     * @param mixed $default The default value to be returned
-     * @return mixed $queryString if it's in the $options array, $default otherwise
-     */
-    private function processQueryString(array $options, $default, $queryString) 
-    {
-        return in_array($queryString, $options) ? $queryString : $default;
-    }
-
-    public function index(Request $request)
+    public function index(\App\Http\Requests\SortTickets $request)
     {      
-        $sortBy = $this->processQueryString(
-            array('created_at', 'due_date'), 
-            'created_at', 
-            $request->query('sort-by', null)
-        );
-        $orderBy = $this->processQueryString(
-            array('asc', 'desc'), 
-            'asc', 
-            $request->query('order-by', null)
-        );
-        $perPage = $this->processQueryString(
-            array(5, 10, 15), 
-            15, 
-            $request->query('per-page', null)
-        );
+        $sortBy = $request->query('sort-by', 'created_at');
+        $orderBy = $request->query('order-by', 'asc');
+        $perPage = $request->query('per-page', 15);
 
         $tickets = Ticket::with('customer')
             ->orderBy($sortBy, $orderBy)
