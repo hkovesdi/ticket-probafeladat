ec566967a097a4558d30bf773960a2c807cc8698
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 10:43:57 +0200

    create SortTickets request

---
+++ app/Http/Requests/SortTickets.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SortTickets extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user() !== null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sort-by' => 'sometimes|in:created_at,due_date',
+            'order-by' => 'sometimes|in:asc,desc',
+            'per-page' => 'sometimes|in:5,10,15'
+        ];
+    }
+}
