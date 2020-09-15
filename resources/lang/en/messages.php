6e96920f1f4962a284f91e90bcc09033b7ed3142
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 15:56:35 +0200

    add english localization of create ticket and menu

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,5 +4,28 @@ return [
     'loginSuccess' => 'Successfully logged in',
     'loginError' => 'Wrong username or password',
     'logoutSuccess' => 'Successfully logged out',
-    'storeTicketSuccess' => 'Ticket successfully created',
+    'menu' => [
+        'title' => 'Ticket system',
+        'login' => 'Login',
+        'logout' => 'Logout',
+        'newTicket' => 'Submit new ticket',
+        'tickets' => 'Tickets',
+        'customers' => 'Customers',
+        'sortOptions' => 'Sort options'
+    ],
+    'tickets' => [
+        'create' => [
+            'form' => [
+                'name' => 'Submit new ticket',
+                'inputs' => [
+                    'name' => 'Name',
+                    'email' => 'Email',
+                    'title' => 'Title',
+                    'content' => 'Content',
+                    'submit' => 'Submit'
+                ]
+            ],
+            'success' => 'Ticket successfully submitted'
+        ]
+    ]
 ];
