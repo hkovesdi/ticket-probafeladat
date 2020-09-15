5bd844655518b05f250c4a0206b23caf41e35335
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 14:57:25 +0200

    add hungarian messages

---
+++ resources/lang/hu/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'loginSuccess' => 'Sikeresen bejelentkezve',
+    'loginError' => 'Rossz felhasználónév vagy jelszó',
+    'logoutSuccess' => 'Sikeresen kijelentkezve',
+    'storeTicketSuccess' => 'Hibajegy sikeresen létrehozva'
+];
