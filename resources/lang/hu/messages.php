011fb9317202f23baec0d91e95e4684ee869ff34
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 15:56:49 +0200

    add hungarian localization of create ticket and menu

--- resources/lang/hu/messages.php
+++ resources/lang/hu/messages.php
@@ -4,5 +4,28 @@ return [
     'loginSuccess' => 'Sikeresen bejelentkezve',
     'loginError' => 'Rossz felhasználónév vagy jelszó',
     'logoutSuccess' => 'Sikeresen kijelentkezve',
-    'storeTicketSuccess' => 'Hibajegy sikeresen létrehozva'
+    'menu' => [
+        'title' => 'Ticket rendszer',
+        'login' => 'Bejelentkezés',
+        'logout' => 'Kijelentkezés',
+        'newTicket' => 'Új hibajegy beküldése',
+        'tickets' => 'Hibajegyek',
+        'customers' => 'Ügyfelek',
+        'sortOptions' => 'Rendezési beállítások'
+    ],
+    'tickets' => [
+        'create' => [
+            'form' => [
+                'name' => 'Új hibajegy beküldése',
+                'inputs' => [
+                    'name' => 'Név',
+                    'email' => 'Email',
+                    'title' => 'Tárgy',
+                    'content' => 'Tartalom',
+                    'submit' => 'Beküldés'
+                ]
+            ],
+            'success' => 'Hibajegy sikeresen beküldve'
+        ]
+    ]
 ];
