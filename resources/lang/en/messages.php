a315c0a4892a088957d1b4d0220982dd6ea2d674
hkovesdi <kovesdi.hunor99@gmail.com>
2020-09-15 14:57:16 +0200

    add english messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'loginSuccess' => 'Successfully logged in',
+    'loginError' => 'Wrong username or password',
+    'logoutSuccess' => 'Successfully logged out',
+    'storeTicketSuccess' => 'Ticket successfully created',
+];
